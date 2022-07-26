c5d04829a4fa5b059aa430f9d468e269af578593
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-07-26 12:11:36 +0200

    Fixed fetching Contacts from database

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -7,98 +7,17 @@ const Contacts = (props) => {
   return(
     <div className={styles.contactsSection}>
       <ScrollToBottom className={styles.contactScroll}>
-
-      {props.contacts.map((contact) => {
-        <div className={styles.singleContact}>
-        <div className={styles.emailContact}>
-          <p>{contact.id}</p>
-        </div>
-        <div className={styles.messageContact}>
-          <p>{contact.message}</p>
-        </div>
-      </div>
-      })}
-        {/* <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div>
-
-        <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div>
-
-        <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div>
-
-        <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div>
-
-        <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div>
-
-        <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div>
-
-        <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div>
-
-        <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div>
-
-        <div className={styles.singleContact}>
-          <div className={styles.emailContact}>
-            <p>example@example.com</p>
-          </div>
-          <div className={styles.messageContact}>
-            <p>Hello! I have a question about party on your Alpaca Farm.</p>
-          </div>
-        </div> */}
-
+        {props.data.contacts.map((contact) => {
+          return (
+          <div className={styles.singleContact} key={contact.id}>
+            <div className={styles.emailContact}>
+              <p>{contact.id}</p>
+            </div>
+            <div className={styles.messageContact}>
+              <p>{contact.messages[contact.messages.length - 1].message}</p>
+            </div>
+          </div>)
+        })}
       </ScrollToBottom>
     </div>
   )
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,41 +1,60 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Contacts from "../components/Contacts";
 import ChatContainer from "../components/ChatContainer";
 
 import styles from "../components/admin.module.css"
 
+function parse(obj) {
+ let tmp_arr = []
+ let tmp_arr_msg = []
+  for (const key in obj) {
+    let elem = {}
+    elem.id = key
+    for (const key1 in obj[key].msgs) {
+      let message = {}
+      let tmp = obj[key].msgs[key1]
+      // console.log(tmp)
+      message.author = tmp.author
+      message.message = tmp.message
+      message.time = tmp.time
+      tmp_arr_msg.push(message)
+    }
+    elem.messages = tmp_arr_msg
+    tmp_arr_msg = []
+    // console.log(elem)
+    tmp_arr.push(elem)
+  }
+  return tmp_arr
+}
+
 const Admin = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState();
   const [currentChat, setCurrentChat] = useState(undefined);
   const [error, setError] = useState(null);
 
-  const fetchContactsHandler = useCallback(async () => {
+  const fetchContactsHandler = async () => {
     setError(null);
     try {
       const response = await fetch('https://chat-app-a4821-default-rtdb.firebaseio.com/chats.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
-
+      let map1 = []
       const data = await response.json();
-      console.log(response)
 
-      const transformedContacts = data.map((contact) => {
-        return {
-          id: contact,
-          message: contact.msgs.message,
-        };
-      });
-      setContacts(transformedContacts);
+      map1 = parse(data)
+      let tmp = {
+        contacts:  map1
+      }
+      setContacts(tmp);
     } catch (error) {
       setError(error.message);
     }
-  }, []);
+  };
 
   useEffect(() => {
     fetchContactsHandler();
-    console.log("aaaaaaaaaaaaaa")
   }, []);
 
   const handleChatChange = (chat) => {
@@ -46,7 +65,7 @@ const Admin = () => {
     <div className={styles.adminSection}>
       <p className={styles.title}>Alpaca Farm Admin Panel</p>
       <div className={styles.adminContainer}>
-        <Contacts contacts={contacts} changeChat={handleChatChange} />
+        {contacts && <Contacts data={contacts} changeChat={handleChatChange} />}
         <ChatContainer currentChat={currentChat} />
       </div>
     </div>
